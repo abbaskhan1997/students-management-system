b2152d4e58648c0cdc1d856c84b4f36b6d507c19
abbaskhan1997 <abbaskhancs88@gmail.com>
2026-01-30 12:07:44 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace StudentManagementSystem
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            this.btninsert = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.Label();
+            this.txtCGPA = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnview = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btninsert
+            // 
+            this.btninsert.BackColor = System.Drawing.Color.SpringGreen;
+            this.btninsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.btninsert.Location = new System.Drawing.Point(359, 275);
+            this.btninsert.Name = "btninsert";
+            this.btninsert.Size = new System.Drawing.Size(154, 73);
+            this.btninsert.TabIndex = 12;
+            this.btninsert.Text = "Insert";
+            this.btninsert.UseVisualStyleBackColor = false;
+            this.btninsert.Click += new System.EventHandler(this.btninsert_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(307, 109);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 42);
+            this.textBox1.TabIndex = 11;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.txtName.Location = new System.Drawing.Point(174, 109);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(125, 40);
+            this.txtName.TabIndex = 10;
+            this.txtName.Text = "Name";
+            this.txtName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtCGPA
+            // 
+            this.txtCGPA.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.txtCGPA.Location = new System.Drawing.Point(625, 191);
+            this.txtCGPA.Name = "txtCGPA";
+            this.txtCGPA.Size = new System.Drawing.Size(125, 40);
+            this.txtCGPA.TabIndex = 9;
+            this.txtCGPA.Text = "CGPA";
+            this.txtCGPA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.txtAge.Location = new System.Drawing.Point(174, 191);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(125, 40);
+            this.txtAge.TabIndex = 8;
+            this.txtAge.Text = "Age";
+            this.txtAge.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.txtEmail.Location = new System.Drawing.Point(616, 109);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(125, 40);
+            this.txtEmail.TabIndex = 7;
+            this.txtEmail.Text = "Email";
+            this.txtEmail.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(307, 191);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(270, 42);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(749, 107);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(270, 42);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(749, 189);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(270, 42);
+            this.textBox4.TabIndex = 4;
+            // 
+            // btnview
+            // 
+            this.btnview.BackColor = System.Drawing.Color.SlateGray;
+            this.btnview.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.btnview.Location = new System.Drawing.Point(553, 275);
+            this.btnview.Name = "btnview";
+            this.btnview.Size = new System.Drawing.Size(154, 73);
+            this.btnview.TabIndex = 3;
+            this.btnview.Text = "View";
+            this.btnview.UseVisualStyleBackColor = false;
+            this.btnview.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnupdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.btnupdate.Location = new System.Drawing.Point(737, 275);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(154, 73);
+            this.btnupdate.TabIndex = 2;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = false;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Silver;
+            this.dataGridView1.Location = new System.Drawing.Point(242, 383);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(777, 222);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(508, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(488, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Management System\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1460, 617);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnview);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtCGPA);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btninsert);
+            this.Name = "Form1";
+            this.Text = "Student Registration";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Button btninsert;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label txtName;
+        private System.Windows.Forms.Label txtCGPA;
+        private System.Windows.Forms.Label txtAge;
+        private System.Windows.Forms.Label txtEmail;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnview;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,297 @@
+﻿using Npgsql;
+using System;
+using System.Data;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace StudentManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        private int selectedRowIndex = -1;
+        private readonly string connectionString =
+            "Host=localhost;Port=5432;Database=studentrecord;Username=postgres;Password=password";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadStudents();
+            dataGridView1.CellClick += DataGridView1_CellClick;
+        }
+
+        #region DataGridView Button Columns
+        private void AddUpdateButtonColumn()
+        {
+            if (!dataGridView1.Columns.Contains("btnUpdate"))
+            {
+                var btn = new DataGridViewButtonColumn
+                {
+                    HeaderText = "Action",
+                    Name = "btnUpdate",
+                    Text = "Edit",
+                    UseColumnTextForButtonValue = true,
+                    DisplayIndex = dataGridView1.Columns.Count
+                };
+                dataGridView1.Columns.Add(btn);
+            }
+        }
+
+        private void AddDeleteButtonColumn()
+        {
+            if (!dataGridView1.Columns.Contains("btnDelete"))
+            {
+                var btn = new DataGridViewButtonColumn
+                {
+                    HeaderText = "Action",
+                    Name = "btnDelete",
+                    Text = "Delete",
+                    UseColumnTextForButtonValue = true,
+                    DisplayIndex = dataGridView1.Columns.Count
+                };
+                dataGridView1.Columns.Add(btn);
+            }
+        }
+        #endregion
+
+        #region DataGridView Events
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            // EDIT BUTTON
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "btnUpdate")
+            {
+                selectedRowIndex = e.RowIndex;
+                LoadSelectedRowToTextBoxes(e.RowIndex);
+            }
+
+            // DELETE BUTTON
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "btnDelete")
+            {
+                string email = dataGridView1.Rows[e.RowIndex].Cells["Email"].Value.ToString();
+                var confirm = MessageBox.Show(
+                    "Are you sure you want to delete this record?",
+                    "Confirm Delete",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning);
+
+                if (confirm == DialogResult.Yes)
+                {
+                    DeleteStudent(email);
+                    LoadStudents();
+                }
+            }
+        }
+
+        private void LoadSelectedRowToTextBoxes(int rowIndex)
+        {
+            textBox1.Text = dataGridView1.Rows[rowIndex].Cells["Name"].Value?.ToString();
+            textBox2.Text = dataGridView1.Rows[rowIndex].Cells["Email"].Value?.ToString();
+            textBox3.Text = dataGridView1.Rows[rowIndex].Cells["Age"].Value?.ToString();
+            textBox4.Text = dataGridView1.Rows[rowIndex].Cells["CGPA"].Value?.ToString();
+        }
+        #endregion
+
+        #region Validation
+        private bool ValidateInputs(out int age, out decimal cgpa)
+        {
+            age = 0;
+            cgpa = 0;
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Name is required.");
+                textBox1.Focus();
+                return false;
+            }
+
+            if (!Regex.IsMatch(textBox1.Text.Trim(), @"^[A-Za-z ]+$"))
+            {
+                MessageBox.Show("Name must contain only alphabets.");
+                textBox1.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Email is required.");
+                textBox2.Focus();
+                return false;
+            }
+
+            if (!Regex.IsMatch(textBox2.Text.Trim(), @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                MessageBox.Show("Invalid email format.");
+                textBox2.Focus();
+                return false;
+            }
+
+            if (!int.TryParse(textBox3.Text.Trim(), out age) || age < 18 || age > 60)
+            {
+                MessageBox.Show("Enter valid age (18-60).");
+                textBox3.Focus();
+                return false;
+            }
+
+            if (!decimal.TryParse(textBox4.Text.Trim(), out cgpa) || cgpa < 0 || cgpa > 4)
+            {
+                MessageBox.Show("Enter valid CGPA (0-4).");
+                textBox4.Focus();
+                return false;
+            }
+
+            if (decimal.Round(cgpa, 2) != cgpa)
+            {
+                MessageBox.Show("CGPA can have only 1 or 2 digits after decimal.");
+                textBox4.Focus();
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
+
+        #region CRUD Operations
+        private void btninsert_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInputs(out int age, out decimal cgpa)) return;
+
+            try
+            {
+                using (var con = new NpgsqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    using (var cmdCheck = new NpgsqlCommand(
+                        "SELECT COUNT(*) FROM students WHERE LOWER(email)=LOWER(@Email)", con))
+                    {
+                        cmdCheck.Parameters.AddWithValue("@Email", textBox2.Text.Trim());
+                        if (Convert.ToInt32(cmdCheck.ExecuteScalar()) > 0)
+                        {
+                            MessageBox.Show("Email already exists.");
+                            textBox2.Focus();
+                            return;
+                        }
+                    }
+
+                    using (var cmd = new NpgsqlCommand(
+                        "INSERT INTO students(name,email,age,cgpa) VALUES(@Name,@Email,@Age,@Cgpa)", con))
+                    {
+                        cmd.Parameters.AddWithValue("@Name", textBox1.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Email", textBox2.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Age", age);
+                        cmd.Parameters.AddWithValue("@Cgpa", cgpa);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                MessageBox.Show("Record inserted successfully ✅");
+                ClearTextBoxes();
+                LoadStudents();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            if (selectedRowIndex < 0)
+            {
+                MessageBox.Show("Select a record from the grid first.");
+                return;
+            }
+
+            if (!ValidateInputs(out int age, out decimal cgpa)) return;
+
+            try
+            {
+                using (var con = new NpgsqlConnection(connectionString))
+                {
+                    using (var cmd = new NpgsqlCommand(
+                        "UPDATE students SET name=@Name, age=@Age, cgpa=@Cgpa WHERE LOWER(email)=LOWER(@Email)", con))
+                    {
+                        cmd.Parameters.AddWithValue("@Name", textBox1.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Age", age);
+                        cmd.Parameters.AddWithValue("@Cgpa", cgpa);
+                        cmd.Parameters.AddWithValue("@Email", textBox2.Text.Trim());
+
+                        con.Open();
+                        int rows = cmd.ExecuteNonQuery();
+
+                        if (rows > 0)
+                        {
+                            MessageBox.Show("Record updated successfully ✅");
+                            LoadStudents();
+                            ClearTextBoxes();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No record found with this email ❌");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void DeleteStudent(string email)
+        {
+            try
+            {
+                using (var con = new NpgsqlConnection(connectionString))
+                {
+                    using (var cmd = new NpgsqlCommand(
+                        "DELETE FROM students WHERE LOWER(email)=LOWER(@Email)", con))
+                    {
+                        cmd.Parameters.AddWithValue("@Email", email.Trim());
+                        con.Open();
+                        int rows = cmd.ExecuteNonQuery();
+
+                        MessageBox.Show(rows > 0
+                            ? "Record deleted successfully ✅"
+                            : "Record not found ❌");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void LoadStudents()
+        {
+            using (var con = new NpgsqlConnection(connectionString))
+            {
+                con.Open();
+                var da = new NpgsqlDataAdapter("SELECT * FROM students", con);
+                var dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                AddUpdateButtonColumn();
+                AddDeleteButtonColumn();
+            }
+        }
+        #endregion
+
+        private void ClearTextBoxes()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox1.Focus();
+        }
+    }
+}
